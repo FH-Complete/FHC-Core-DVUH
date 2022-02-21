b808caae7d4d5a62794408377fc1b893087f56e5
KarpAlex <karpen_ko@hotmail.com>
2022-02-21 14:32:54 +0100

    jqm scheduler sendstudydata bugfix: use current_date function instead of now() for check if executede today

--- libraries/JQMSchedulerLib.php
+++ libraries/JQMSchedulerLib.php
@@ -441,7 +441,7 @@ class JQMSchedulerLib
 					) prestudents
 					JOIN public.tbl_studiensemester sem USING (studiensemester_kurzbz)
 					WHERE max_studiumdaten_meldedatum IS NULL /* either not sent to DVUH or data modified since last send */
-					OR prestudent_status_datum = NOW() /* if prestudent status gets active today */
+					OR prestudent_status_datum = CURRENT_DATE /* if prestudent status gets active today */
 					OR bisio_endedatum = CURRENT_DATE /* if bisio ende is today, because mobilitaeten in future are sent with no endedatum */
 					OR pss_insertamum >= max_studiumdaten_meldedatum OR ps_insertamum >= max_studiumdaten_meldedatum
 					OR mob_insertamum >= max_studiumdaten_meldedatum OR bisio_insertamum >= max_studiumdaten_meldedatum
