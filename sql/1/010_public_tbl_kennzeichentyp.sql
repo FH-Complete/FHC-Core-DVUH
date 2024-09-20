68b04f7e89eacec985e906b96640aab23e297e53
Alexei Karpenko <karpen_ko@hotmail.com>
2024-09-20 15:52:19 +0200

    added transparenzdatenbank Kennzeichentyp

--- sql/1/010_public_tbl_kennzeichentyp.sql
+++ sql/1/010_public_tbl_kennzeichentyp.sql
@@ -1,4 +1,5 @@
 INSERT INTO public.tbl_kennzeichentyp (kennzeichentyp_kurzbz, bezeichnung, aktiv) VALUES
 ('vbpkAs', 'Statistik vBpk', TRUE),
-('vbpkBf', 'Bildung vBpk', TRUE)
+('vbpkBf', 'Bildung vBpk', TRUE),
+('vbpkBmf', 'Transparenzdatenbank vBpk', TRUE)
 ON CONFLICT (kennzeichentyp_kurzbz) DO NOTHING;
