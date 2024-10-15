591d3711b64d1df91a5a6ff822edfdcd7f0ab47f
Alexei Karpenko <karpen_ko@hotmail.com>
2024-10-15 13:29:11 +0200

    changed Transparenzdatenbank vBPK Type name

--- sql/1/010_public_tbl_kennzeichentyp.sql
+++ sql/1/010_public_tbl_kennzeichentyp.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.tbl_kennzeichentyp (kennzeichentyp_kurzbz, bezeichnung, aktiv) VALUES
 ('vbpkAs', 'Statistik vBpk', TRUE),
 ('vbpkBf', 'Bildung vBpk', TRUE),
-('vbpkBmf', 'Transparenzdatenbank vBpk', TRUE)
+('vbpkTd', 'Transparenzdatenbank vBpk', TRUE)
 ON CONFLICT (kennzeichentyp_kurzbz) DO NOTHING;
