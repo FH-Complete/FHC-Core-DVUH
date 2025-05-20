1eb2dc90defd52c265e17db0e1b92352533a9d93
Andreas Österreicher <oesi@technikum-wien.at>
2025-05-20 11:55:41 +0200

    Für Bewerber werden nun auch  Default BPKs geholt da diese für die UHSTAT Meldung benötigt werden

--- config/DVUHSync.php.ex
+++ config/DVUHSync.php.ex
@@ -25,8 +25,8 @@ $config['fhc_dvuh_status_kurzbz'] = array(
 	'DVUHSendCharge' => array('Aufgenommener', 'Student', 'Incoming', 'Diplomand', 'Abbrecher', 'Unterbrecher', 'Absolvent'),
 	'DVUHSendPayment' => array('Aufgenommener', 'Student', 'Incoming', 'Diplomand', 'Abbrecher', 'Unterbrecher', 'Absolvent'),
 	'DVUHSendStudyData' => array('Student', 'Incoming', 'Diplomand', 'Abbrecher', 'Unterbrecher', 'Absolvent'),
-	'DVUHGetBpk' => array('Aufgenommener', 'Student', 'Incoming', 'Diplomand'),
-	'DVUHRequestBpk' => array('Aufgenommener', 'Student', 'Incoming', 'Diplomand'),
+	'DVUHGetBpk' => array('Bewerber','Aufgenommener', 'Student', 'Incoming', 'Diplomand'),
+	'DVUHRequestBpk' => array('Bewerber','Aufgenommener', 'Student', 'Incoming', 'Diplomand'),
 	'DVUHRequestEkz' => array('Bewerber', 'Aufgenommener', 'Student', 'Incoming', 'Diplomand'),
 	'DVUHSendPruefungsaktivitaeten' => array('Aufgenommener', 'Student', 'Incoming', 'Diplomand', 'Abbrecher', 'Unterbrecher', 'Absolvent')
 );
