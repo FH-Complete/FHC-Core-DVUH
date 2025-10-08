52c89e559bd15318ca9daac15465d73cd2e12c9d
Alexei Karpenko <karpen_ko@hotmail.com>
2025-10-08 18:10:30 +0200

    issue resolver DVUH_SC_0001: matrikelnr doesn't need to be present if it is only a Abgewiesener

--- libraries/issues/resolvers/DVUH_SC_0001.php
+++ libraries/issues/resolvers/DVUH_SC_0001.php
@@ -13,25 +13,41 @@ class DVUH_SC_0001 implements IIssueResolvedChecker
 			return error('Person Id missing, issue_id: '.$params['issue_id']);
 
 		$this->_ci =& get_instance(); // get code igniter instance
-
 		$this->_ci->load->model('person/Person_model', 'PersonModel');
 
-		// get matrikelnr of person
-		$this->_ci->PersonModel->addSelect('matr_nr');
-		$personRes = $this->_ci->PersonModel->load($params['issue_person_id']);
+		// get student without matrikelnummer, who is not rejected
+		$query = "
+			SELECT 1 FROM
+			(
+				SELECT
+					DISTINCT ON (prestudent_id) prestudent_id, status_kurzbz
+				FROM
+				(
+					SELECT
+						ps.prestudent_id, pss.status_kurzbz, pss.datum, pss.insertamum
+					FROM
+						public.tbl_person p
+						JOIN public.tbl_prestudent ps USING (person_id)
+						JOIN public.tbl_prestudentstatus pss USING (prestudent_id)
+					WHERE
+						(matr_nr IS NULL OR matr_nr = '')
+						AND p.person_id = ?
+				) status
+				ORDER BY
+					prestudent_id, datum DESC, insertamum DESC
+			) letzte_status
+			WHERE
+				status_kurzbz <> 'Abgewiesener'";
+
+		$personRes = $this->_ci->PersonModel->execReadOnlyQuery($query, [$params['issue_person_id']]);
 
 		if (isError($personRes))
 			return $personRes;
 
+		// check if there is an entry without matrikelnummer, issue is not resolved
 		if (hasData($personRes))
-		{
-			// check if matrikelnr exists
-			if (isEmptyString(getData($personRes)[0]->matr_nr))
-				return success(false);
-			else
-				return success(true);
-		}
-		else
 			return success(false);
+		else
+			return success(true); // issue resolved
 	}
 }
